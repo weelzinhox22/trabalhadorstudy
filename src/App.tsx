6c2ce12c6c442116fb1832c72a5e01a43dd9ccdd
wel <weelzinhox22@gmail.com>
2025-04-05 19:25:04 -0300

    Descrição das novas alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import SinanInfo from "./pages/SinanInfo";
 import Quiz from "./pages/Quiz";
 import QuizResults from "./pages/QuizResults";
 import NotFound from "./pages/NotFound";
